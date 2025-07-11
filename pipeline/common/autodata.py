57acdea6b0799768f529c3991613d727f39e2471
reefmap <duncan.hume@gmail.com>
2025-07-11 14:51:39 +0200

    fix issue with asset id

--- pipeline/common/autodata.py
+++ pipeline/common/autodata.py
@@ -2,15 +2,22 @@ import os, requests, zipfile, tempfile, ee
 from pathlib import Path
 
 CACHE = Path(os.getenv("MHP_CACHE_DIR", "~/.mhp_cache")).expanduser()
-ASSET_ID = "users/{}/global_tidal_range".format(
-    ee.data.getAssetRoots()[0]['id'].split('/')[1]
-)
 SRC_ZIP = "https://api.researchdata.se/dataset/ecds0243-1/1.0/file/zip"
+
+# ------------------------------------------------------------------#
+# Helpers                                                           #
+# ------------------------------------------------------------------#
+def _asset_id() -> str:
+    """Compute the tidal-range asset path *after* EE is initialized."""
+    root = ee.data.getAssetRoots()[0]["id"]            # users/<name>
+    user = root.split("/")[1]
+    return f"users/{user}/global_tidal_range"
 
 def ensure_tidal_asset():
     """Download + upload the 5-km tidal-range raster exactly once."""
+    asset = _asset_id()
     try:
-        return ee.Image(ASSET_ID)
+        return ee.Image(asset)
     except Exception:
         pass                     # not found → fall through and ingest
 
@@ -29,9 +36,9 @@ def ensure_tidal_asset():
         tif = os.path.join(td, "annual_max_cycle_amp_cm.tif")
         task_id = ee.data.newTaskId()[0]
         task = ee.data.startIngestion(task_id, {
-            "id": ASSET_ID,
+            "id": asset,
             "tilesets": [{"sources": [{"uris": [tif]}]}],
         })
         ee.batch.Task(task).start()
         ee.batch.Task(task).status()['state']        # blocks
-    return ee.Image(ASSET_ID)
+    return ee.Image(asset)
